e7b9786240f2e8fe185b0afb09d1199c87c2f701
mikkelwillen <mikkel@willen.dk>
2021-09-12 15:38:36 +0200

    tekst ting

--- Aflevering1/file.c
+++ Aflevering1/file.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+
+int print_error(char* path, int errnum) {
+    return fprintf(stdout, "%s: cannont determine (%s)\n", path, strerror(errnum));
+}
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
@@ -14,18 +21,39 @@ int main(int argc, char* argv[]) {
     FILE* file = fopen(inputFile, "r");
     char* fileType;
 
+    int errnoCheck = errno;
+    if (errnoCheck != 0) {
+        print_error(argv[1], errnoCheck);
+        return 0;
+    }
+
+    // if (file == NULL) {
+    //     fprintf(stdout, "No file with that name exists\n");
+    //     return 0;
+    // }
+
     char emptyDocument;
     int read = fread(&emptyDocument, 1, 1, file);
 
     int checkAscii = 0;
+    int checkISO8859 = 0;
 
     while(1) {
         char asciiChar;
         int read = fread(&asciiChar, 1, 1, file);
         int asciiCodeChar = (int) asciiChar;
+        printf(" %d = %c ", asciiCodeChar, asciiChar);
+
+        // skal vi lave errnoChecks over det hele???
         
+
+        // hvad sker der for a,ø,å på bitform??
         if(asciiCodeChar > 128) {
             checkAscii = 1;
+        }
+
+        if(asciiCodeChar > 128 || asciiCodeChar < 159 && asciiCodeChar > 256) {
+            checkISO8859 = 1;
         }
 
         if (read == 0) {
@@ -33,19 +61,18 @@ int main(int argc, char* argv[]) {
         }
     }
 
+
+    // UTF-Unicode, hvad i al verden??
     if (read == 0) {
         fileType = "empty";
     } else if (checkAscii == 0) {
         fileType = "ascii";
+    } else if (checkISO8859 == 0) {
+        fileType = "ISO8859";
     } else {
         fileType = "data";
     }
 
-    if (file == NULL) {
-        fprintf(stdout, "No file with that name exists");
-        return 0;
-    } else {
-        fprintf(stdout, "%s: %s\n", inputFile, fileType);
-        return 0;
-    }
+    fprintf(stdout, "%s: %s\n", inputFile, fileType);
+    return 0;
 }
