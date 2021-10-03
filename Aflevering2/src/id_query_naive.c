412eca37c059eea926f6d6951127ac942a214011
mikkelwillen <mikkel@willen.dk>
2021-10-03 17:32:50 +0200

    ting

---
+++ Aflevering2/src/id_query_naive.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+
+#include "record.h"
+#include "id_query.h"
+
+struct naive_data {
+  struct record *rs;
+  int n;
+};
+
+struct naive_data* mk_naive(struct record* rs, int n) {
+  struct naive_data *nd = malloc(sizeof(struct naive_data));
+  nd->rs = rs;
+  nd->n = n;
+  return nd;
+}
+
+void free_naive(struct naive_data* data) {
+  free(data);
+}
+
+const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
+  for (int i = 0; i < data->n - 1; i ++) {
+    int64_t temp = data->rs[i].osm_id;
+    if (temp == needle) {
+      return data->rs;
+    }
+  }
+  return NULL;
+}
+
+int main(int argc, char** argv) {
+  return id_query_loop(argc, argv,
+                    (mk_index_fn)mk_naive,
+                    (free_index_fn)free_naive,
+                    (lookup_fn)lookup_naive);
+}
