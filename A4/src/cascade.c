b8cc0c5d24b8d17fd12e0edeb4527d9b846c052c
mikkelwillen <mikkel@willen.dk>
2021-11-23 12:50:06 +0100

    fjernet lort

--- A4/src/cascade.c
+++ A4/src/cascade.c
@@ -26,8 +26,7 @@ struct csc_peer *peers;
 /*
  * Frees global resources that are malloc'ed during peer downloads.
  */
-void free_resources()
-{
+void free_resources() {
     free(queue);
     free(peers);
     csc_free_file(casc_file);
@@ -39,18 +38,16 @@ void free_resources()
  * a normal size for the hash would be given by the global variable
  * 'SHA256_HASH_SIZE', that has been defined in sha256.h
  */
-void get_data_sha(const char* sourcedata, hashdata_t hash, uint32_t data_size, int hash_size)
-{
-  SHA256_CTX shactx;
-  unsigned char shabuffer[hash_size];
-  sha256_init(&shactx);
-  sha256_update(&shactx, sourcedata, data_size);
-  sha256_final(&shactx, shabuffer);
+void get_data_sha(const char* sourcedata, hashdata_t hash, uint32_t data_size, int hash_size) {
+    SHA256_CTX shactx;
+    unsigned char shabuffer[hash_size];
+    sha256_init(&shactx);
+    sha256_update(&shactx, sourcedata, data_size);
+    sha256_final(&shactx, shabuffer);
 
-  for (int i=0; i<hash_size; i++)
-    {
-      hash[i] = shabuffer[i];
-    }
+    for (int i = 0; i < hash_size; i++) {
+        hash[i] = shabuffer[i];
+        }
 }
 
 /*
@@ -59,13 +56,11 @@ void get_data_sha(const char* sourcedata, hashdata_t hash, uint32_t data_size, i
  * a normal size for the hash would be given by the global variable
  * 'SHA256_HASH_SIZE', that has been defined in sha256.h
  */
-void get_file_sha(const char* sourcefile, hashdata_t hash, int size)
-{
+void get_file_sha(const char* sourcefile, hashdata_t hash, int size) {
     int casc_file_size;
 
     FILE* fp = Fopen(sourcefile, "rb");
-    if (fp == 0)
-    {
+    if (fp == 0) {
         printf("Failed to open source: %s\n", sourcefile);
         return;
     }
@@ -86,11 +81,9 @@ void get_file_sha(const char* sourcefile, hashdata_t hash, int size)
  * E.g. parse a cascade file and get all the relevent data from somewhere else on the
  * network.
  */
-void download_only_peer(char* cascade_file)
-{
+void download_only_peer(char* cascade_file) {
     printf("Managing download only for: %s\n", cascade_file);
-    if (access(cascade_file, F_OK ) != 0 )
-    {
+    if (access(cascade_file, F_OK ) != 0 ) {
         fprintf(stderr, ">> File %s does not exist\n", cascade_file);
         exit(EXIT_FAILURE);
     }
@@ -98,8 +91,8 @@ void download_only_peer(char* cascade_file)
     char output_file[strlen(cascade_file)];
     memcpy(output_file, cascade_file, strlen(cascade_file));
     char* r = strstr(cascade_file, "cascade");
-    int cutoff = r - cascade_file ;
-    output_file[cutoff-1] = '\0';
+    int cutoff = r - cascade_file;
+    output_file[cutoff - 1] = '\0';
     printf("Downloading to: %s\n", output_file);
 
     casc_file = csc_parse_file(cascade_file, output_file);
@@ -107,18 +100,15 @@ void download_only_peer(char* cascade_file)
     int uncomp_count = 0;
     queue = Malloc(casc_file->blockcount * sizeof(csc_block_t*));
 
-    for (uint64_t i = 0; i < casc_file->blockcount; i++)
-    {
-        if (casc_file->blocks[i].completed == 0)
-        {
+    for (uint64_t i = 0; i < casc_file->blockcount; i++) {
+        if (casc_file->blocks[i].completed == 0) {
             queue[uncomp_count] = &casc_file->blocks[i];
             uncomp_count++;
         }
     }
 
     printf("Missing blocks: %d/%lud\n", uncomp_count, casc_file->blockcount);
-    if (uncomp_count == 0)
-    {
+    if (uncomp_count == 0) {
         printf("All blocks are already present, skipping external connections.\n");
         free_resources();
         return;
@@ -129,35 +119,28 @@ void download_only_peer(char* cascade_file)
     get_file_sha(cascade_file, hash_buf, SHA256_HASH_SIZE);
 
     int peercount = 0;
-    while (peercount == 0)
-    {
+    while (peercount == 0) {
         peercount = get_peers_list(hash_buf);
-        if (peercount == 0)
-        {
+        if (peercount == 0) {
             printf("No peers were found. Will try again in %d seconds\n", PEER_REQUEST_DELAY);
             fflush(stdout);
             sleep(PEER_REQUEST_DELAY);
-        }
-        else
-        {
+        } else {
             printf("Found %d peer(s)\n", peercount);
         }
     }
 
     csc_peer_t peer = peers[0];
     // Get a good peer if one is available
-    for (int i = 0; i < peercount; i++)
-    {
-        if (peers[i].good)
-        {
+    for (int i = 0; i < peercount; i++) {
+        if (peers[i].good) {
             peer = peers[i];
             break;
         }
     }
 
     printf("Downloading blocks\n");
-    for (int i = 0; i < uncomp_count; i++)
-    {
+    for (int i = 0; i < uncomp_count; i++) {
         get_block(queue[i], peer, hash_buf, output_file);
     }
     printf("File downloaded successfully\n");
@@ -168,13 +151,10 @@ void download_only_peer(char* cascade_file)
 /*
  * Count how many times a character occurs in a string
  */
-int count_occurences(char string[], char c)
-{
-    int count=0;
-    for(size_t i = 0; i < strlen(string); i++)
-    {
-        if(string[i] == c)
-        {
+int count_occurences(char string[], char c) {
+    int count = 0;
+    for(size_t i = 0; i < strlen(string); i++) {
+        if(string[i] == c) {
             count++;
         }
     }
@@ -186,33 +166,29 @@ int count_occurences(char string[], char c)
  * Parses a cascade file, given the sourcepath input and destination, which may or may not exist.
  * Returns a pointer to a datastructure describing the file, or NULL if the file could not be parsed
  */
-csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
-{
+csc_file_t* csc_parse_file(const char* sourcefile, const char* destination) {
     FILE* fp = Fopen(sourcefile, "rb");
-    if (fp == 0)
-    {
+    if (fp == 0) {
         printf("Failed to open source: %s\n", sourcefile);
         return NULL;
     }
 
-    const int FILE_HEADER_SIZE = 8+8+8+8+32;
+    const int FILE_HEADER_SIZE = 8 + 8 + 8 + 8 + 32;
 
     char header[FILE_HEADER_SIZE];
-    if (Fread(header, 1, FILE_HEADER_SIZE, fp) != (size_t)FILE_HEADER_SIZE)
-    {
+    if (Fread(header, 1, FILE_HEADER_SIZE, fp) != (size_t)FILE_HEADER_SIZE) {
         printf("Failed to read magic 8 bytes header from file\n");
         Fclose(fp);
         return NULL;
     }
 
-    if (memcmp(header, "CASCADE1", 8) != 0)
-    {
+    if (memcmp(header, "CASCADE1", 8) != 0) {
         printf("File does not contain magic 8 bytes in header\n");
         Fclose(fp);
         return NULL;
     }
 
-    csc_file_t* res = (csc_file_t*)Malloc(sizeof(csc_file_t));
+    csc_file_t* res = (csc_file_t*) Malloc(sizeof(csc_file_t));
     res->targetsize = be64toh(*((unsigned long long*)&header[16]));
     res->blocksize = be64toh(*((unsigned long long*)&header[24]));
     memcpy(&res->targethash, &header[32], 32);
@@ -223,23 +199,20 @@ csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
     res->blockcount = (res->targetsize + res->blocksize - 1) / res->blocksize;
 
     res->blocks = (csc_block_t*)Malloc(sizeof(csc_block_t) * res->blockcount);
-    if (res->blocks == NULL)
-    {
+    if (res->blocks == NULL) {
         printf("Failed to allocate memory for %lud blocks\n", res->blockcount);
         Free(res);
         Fclose(fp);
         return NULL;
     }
 
-    for(uint64_t i = 0; i < res->blockcount; i++)
-    {
+    for(uint64_t i = 0; i < res->blockcount; i++) {
         csc_block_t* b = &res->blocks[i];
         b->index = i;
         b->length = i == res->blockcount - 1 ? res->trailblocksize : res->blocksize;
         b->offset = i * res->blocksize;
         b->completed = 0;
-        if (Fread(&(b->hash), 1, sizeof(hashdata_t), fp) != sizeof(hashdata_t))
-        {
+        if (Fread(&(b->hash), 1, sizeof(hashdata_t), fp) != sizeof(hashdata_t)) {
             printf("Failed to read block %lud/%lud\n", i, res->blockcount);
             csc_free_file(res);
             Fclose(fp);
@@ -250,16 +223,14 @@ csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
     Fclose(fp);
 
     fp = Fopen(destination, "a+w");
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         printf("Failed to open destination file %s\n", destination);
         csc_free_file(res);
         return NULL;
     }
 
     void* buffer = Malloc(res->blocksize);
-    if (buffer == NULL)
-    {
+    if (buffer == NULL) {
         printf("No block buffer asigned: %lud\n", res->blocksize);
         csc_free_file(res);
         Fclose(fp);
@@ -267,12 +238,10 @@ csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
     }
 
     SHA256_CTX shactx;
-    for(uint64_t i = 0; i < res->blockcount; i++)
-    {
+    for(uint64_t i = 0; i < res->blockcount; i++) {
         hashdata_t shabuffer;
         uint64_t size = res->blocks[i].length;
-        if (fread(buffer, size, 1, fp) != 1)
-        {
+        if (fread(buffer, size, 1, fp) != 1) {
             break;
         }
 
@@ -280,8 +249,7 @@ csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
         sha256_update(&shactx, buffer, size);
         sha256_final(&shactx, shabuffer);
 
-        if (memcmp(&res->blocks[i].hash, shabuffer, SHA256_HASH_SIZE) != 0)
-        {
+        if (memcmp(&res->blocks[i].hash, shabuffer, SHA256_HASH_SIZE) != 0) {
             break;
         }
 
@@ -295,8 +263,7 @@ csc_file_t* csc_parse_file(const char* sourcefile, const char* destination)
 /*
  * Releases the memory allocated by a file datastructure
  */
-void csc_free_file(csc_file_t* file)
-{
+void csc_free_file(csc_file_t* file) {
     Free(file->blocks);
     file->blocks = NULL;
     Free(file);
@@ -306,8 +273,7 @@ void csc_free_file(csc_file_t* file)
  * Get a specified block from a peer on the network. The block is retrieved and then inserted directly into
  * the appropriate data file at the appropriate location.
  */
-void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* output_file)
-{
+void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* output_file) {
 
     rio_t rio;
     char msg_buf[MAXLINE];
@@ -331,26 +297,16 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
     char reply_header[PEER_RESPONSE_HEADER_SIZE];
     memcpy(reply_header, msg_buf, PEER_RESPONSE_HEADER_SIZE);
 
-    if (reply_header[0] != 0)
-    {
-        if (reply_header[0] == 1)
-        {
+    if (reply_header[0] != 0) {
+        if (reply_header[0] == 1) {
             printf("Invalid hash. File not present on peer\n");
-        }
-        else if (reply_header[0] == 2)
-        {
+        } else if (reply_header[0] == 2) {
             printf("Invalid hash. Block not present on peer\n");
-        }
-        else if (reply_header[0] == 3)
-        {
+        } else if (reply_header[0] == 3) {
             printf("Invalid block number\n");
-        }
-        else if (reply_header[0] == 4)
-        {
+        } else if (reply_header[0] == 4) {
             printf("Request could not be parsed\n");
-        }
-        else
-        {
+        } else {
             printf("Invalid error code\n");
         }
         Close(peer_socket);
@@ -359,8 +315,7 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
 
     uint64_t msglen = be64toh(*(uint64_t*)&reply_header[1]);
 
-    if (msglen != block->length)
-    {
+    if (msglen != block->length) {
         printf("Invalid block length supplied\n");
         Close(peer_socket);
         return;
@@ -376,10 +331,8 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
     uint64_t to_read = msglen - write_count;
     uint64_t was_read = 0;
 
-    while (write_count < msglen)
-    {
-        if (to_read > MAXLINE)
-        {
+    while (write_count < msglen) {
+        if (to_read > MAXLINE) {
             to_read = MAXLINE;
         }
 
@@ -395,8 +348,7 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
 
     // Also make sure to check block hash is expected
     for (int i = 0; i < SHA256_HASH_SIZE; ++i) {
-        if (block_hash[i] != block->hash[i])
-        {
+        if (block_hash[i] != block->hash[i]) {
             printf("Invalid block hash\n");
             Free(block_data);
             Close(peer_socket);
@@ -405,8 +357,7 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
     }
 
     FILE* fp = Fopen(output_file, "rb+");
-    if (fp == 0)
-    {
+    if (fp == 0) {
         printf("Failed to open destination: %s\n", output_file);
         free(block_data);
         Close(peer_socket);
@@ -424,8 +375,7 @@ void get_block(csc_block_t* block, csc_peer_t peer, hashdata_t hash, char* outpu
  * Get a list of peers on the network from a tracker. Note that this query is doing double duty according to
  * the protocol, and by asking for a list of peers we are also enrolling on the network ourselves.
  */
-int get_peers_list(hashdata_t hash)
-{
+int get_peers_list(hashdata_t hash) {
     rio_t rio;
     char msg_buf[MAXLINE];
 
@@ -465,8 +415,7 @@ int get_peers_list(hashdata_t hash)
 
     if (reply_header[0] != 0) {
         char* error_buf = Malloc(msglen + 1);
-        if (error_buf == NULL)
-        {
+        if (error_buf == NULL) {
             printf("Tracker error %d and out-of-memory reading error\n", reply_header[0]);
             Close(tracker_socket);
             return 0;
@@ -479,8 +428,7 @@ int get_peers_list(hashdata_t hash)
         return 0;
     }
 
-    if (msglen % 12 != 0)
-    {
+    if (msglen % 12 != 0) {
         printf("LIST response from tracker was length %ud but should be evenly divisible by 12\n", msglen);
         Close(tracker_socket);
         return 0;
@@ -493,8 +441,7 @@ int get_peers_list(hashdata_t hash)
     peercount = (uint32_t)(msglen / 12);
     peers = Malloc(sizeof(csc_peer_t) * peercount);
 
-    for(int i = 0; i < peercount; i++)
-    {
+    for(int i = 0; i < peercount; i++) {
         uint8_t peerdata[12];
         memcpy(peerdata, body+(12*i), 12);        
                
@@ -525,30 +472,20 @@ int get_peers_list(hashdata_t hash)
 /*
  * The entry point for the code. Parses command line arguments and starts up the appropriate peer code.
  */
-int main(int argc, char **argv)
-{
-    if (argc != MAIN_ARGNUM + 1)
-    {
+int main(int argc, char **argv) {
+    if (argc != MAIN_ARGNUM + 1) {
         fprintf(stderr, "Usage: %s <cascade file(s)> <tracker server ip> <tracker server port> <peer ip> <peer port>.\n", argv[0]);
         exit(EXIT_FAILURE);
-    }
-    else if (!is_valid_ip(argv[2]))
-    {
+    } else if (!is_valid_ip(argv[2])) {
         fprintf(stderr, ">> Invalid tracker IP: %s\n", argv[2]);
         exit(EXIT_FAILURE);
-    }
-    else if (!is_valid_port(argv[3]))
-    {
+    } else if (!is_valid_port(argv[3])) {
         fprintf(stderr, ">> Invalid tracker port: %s\n", argv[3]);
         exit(EXIT_FAILURE);
-    }
-    else if (!is_valid_ip(argv[4]))
-    {
+    } else if (!is_valid_ip(argv[4])) {
         fprintf(stderr, ">> Invalid peer IP: %s\n", argv[4]);
         exit(EXIT_FAILURE);
-    }
-    else if (!is_valid_port(argv[5]))
-    {
+    } else if (!is_valid_port(argv[5])) {
         fprintf(stderr, ">> Invalid peer port: %s\n", argv[5]);
         exit(EXIT_FAILURE);
     }
@@ -559,32 +496,27 @@ int main(int argc, char **argv)
     snprintf(my_port, PORT_LEN, "%s", argv[5]);
 
     char cas_str[strlen(argv[1])];
-    snprintf(cas_str, strlen(argv[1])+1, "%s", argv[1]);
+    snprintf(cas_str, strlen(argv[1]) + 1, "%s", argv[1]);
     char delim[] = ":";
 
     int casc_count = count_occurences(argv[1], ':') + 1;
-    char *cascade_files[casc_count];
+    char* cascade_files[casc_count];
 
-    char *ptr = strtok(cas_str, delim);
+    char* ptr = strtok(cas_str, delim);
     int i = 0;
 
-    while (ptr != NULL)
-    {
-        if (strstr(ptr, ".cascade") != NULL)
-        {
+    while (ptr != NULL) {
+        if (strstr(ptr, ".cascade") != NULL) {
             cascade_files[i++] = ptr;
             ptr = strtok(NULL, delim);
-        }
-        else
-        {
+        } else {
             printf("Abort on %s\n", ptr);
             fprintf(stderr, ">> Invalid cascade file: %s\n", ptr);
             exit(EXIT_FAILURE);
         }
     }
 
-    for (int j=0; j<casc_count; j++)
-    {
+    for (int j = 0; j < casc_count; j++) {
         download_only_peer(cascade_files[j]);
     }
 
